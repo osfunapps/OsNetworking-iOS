98d1b0759f1a20a25ab36484c75a92bfee3140fd
osfunapps <osfunapps@gmail.com>
2021-07-27 12:15:23 +0300

    s

--- Classes/NetworkTools.swift
+++ Classes/NetworkTools.swift
@@ -23,6 +23,10 @@ public class NetworkTools {
         let startRunning = ipv6Parts.count - 4
         let endRunning = ipv6Parts.count - 1
         
+        if startRunning <= 0 || endRunning <= 0 || startRunning > endRunning {
+            return nil
+        }
+        
         for ipv6PartIdx in startRunning...endRunning {
             var ipv6Part = ipv6Parts[ipv6PartIdx]
             while ipv6Part.count < 4 {
@@ -30,20 +34,53 @@ public class NetworkTools {
             }
             
             let str = String(ipv6Part).substring(0, 2)
-            macParts.append(str)
+            
             let totalIPv6PartSize = ipv6Part.count
             let startIPv6Part = totalIPv6PartSize - 2
-            let m = String(ipv6Part).substring(startIPv6Part, totalIPv6PartSize)
-            macParts.append(m)
+            if totalIPv6PartSize >= startIPv6Part {
+                let m = String(ipv6Part).substring(startIPv6Part, totalIPv6PartSize)
+                macParts.append(str)
+                macParts.append(m)
+            }
         }
         //  modify parts to match MAC value
+        if macParts.isEmpty {
+            return nil
+        }
         guard let ans = Int(macParts[0], radix: 16) else {return nil}
         let ans2 = ans ^ 2
         macParts[0] = String(format:"%02X", ans2)
-        macParts.remove(at: 4)
-        macParts.remove(at: 3)
-        let ans44 = macParts.joined(separator: ":")
-        return ans44
-        
+        if macParts.count > 4 {
+            macParts.remove(at: 4)
+        }
+        if macParts.count > 3 {
+            macParts.remove(at: 3)
+        }
+        let macAddrStr = macParts.joined(separator: ":")
+        if isMACAddressValid(macAddressString: macAddrStr) {
+            return macAddrStr
+        } else {
+            return nil
+        }
     }
+    
+    public static func isMACAddressValid(macAddressString: String) -> Bool {
+         var returnValue = true
+         let macRegEx = "^([0-9A-Fa-f]{2}[:]){5}([0-9A-Fa-f]{2})$" // Format Only: XX:XX:XX:XX:XX:XX
+         //let macRegEx = "^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$" // Format: XX:XX:XX:XX:XX:XX and XX-XX-XX-XX-XX-XX
+         do {
+             let regex = try NSRegularExpression(pattern: macRegEx)
+             let nsString = macAddressString as NSString
+             let results = regex.matches(in: macAddressString, range: NSRange(location: 0, length: nsString.length))
+             
+             if results.count == 0
+             {
+                 returnValue = false
+             }
+         } catch let error as NSError {
+             print("invalid regex: \(error.localizedDescription)")
+             returnValue = false
+         }
+         return  returnValue
+     }
 }
